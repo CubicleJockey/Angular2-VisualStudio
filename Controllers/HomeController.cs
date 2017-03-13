a21867e4c872a249e286bab23f8d620dc9f3d0bf
André Davis <CubicleJockey@users.noreply.github.com>
2017-03-13 11:00:56 -0700

    Cleanup before moving forward

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,10 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
-namespace Angular2_VS_QuickStart.Controllers
+namespace Angular2App.Controllers
 {
     public class HomeController : Controller
     {
--- Controllers/SampleDataController.cs
+++ Controllers/SampleDataController.cs
@@ -1,15 +1,15 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Angular2App.Models;
 
-namespace Angular2_VS_QuickStart.Controllers
+namespace Angular2App.Controllers
 {
     [Route("api/[controller]")]
     public class SampleDataController : Controller
     {
-        private static string[] Summaries = new[]
+        private static readonly string[] Summaries = new[]
         {
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
@@ -24,21 +24,6 @@ namespace Angular2_VS_QuickStart.Controllers
                 TemperatureC = rng.Next(-20, 55),
                 Summary = Summaries[rng.Next(Summaries.Length)]
             });
-        }
-
-        public class WeatherForecast
-        {
-            public string DateFormatted { get; set; }
-            public int TemperatureC { get; set; }
-            public string Summary { get; set; }
-
-            public int TemperatureF
-            {
-                get
-                {
-                    return 32 + (int)(TemperatureC / 0.5556);
-                }
-            }
         }
     }
 }
