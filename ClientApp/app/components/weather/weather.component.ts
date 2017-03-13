1402708b5e1133e44e7d66590e4abc12e9aec4d2
André Davis <CubicleJockey@users.noreply.github.com>
2017-03-13 12:32:48 -0700

    New component

---
+++ ClientApp/app/components/weather/weather.component.ts
@@ -0,0 +1,23 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+    selector: 'weather',
+    templateUrl: './weather.component.html'
+})
+export class WeatherComponent {
+    public title: string;
+    public weather: IWeather;
+
+    public constructor() {
+        const self = this;
+
+        self.title = 'Weather Check';
+        self.weather = { temp: '68', summary: 'Okay weather in Washington', city: 'Kirkland' };
+    }
+}
+
+interface IWeather {
+    temp: string;
+    summary: string;
+    city: string;
+}
